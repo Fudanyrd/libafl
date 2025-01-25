07517b82889d8eaa41a9ff259619a615b4c526f5
anonymous <foo@bar.baz>
2025-01-25 06:17:46 +0000

    Add skeleton for libfuzzer_binutils

---
+++ fuzzers/inprocess/libfuzzer_binutils/src/lib.rs
@@ -0,0 +1,407 @@
+//! A libfuzzer-like fuzzer with llmp-multithreading support and restarts
+//! The example harness is built for binutils-2.34.
+//! In this example, you will see the use of the `launcher` feature.
+//! The `launcher` will spawn new processes for each cpu core.
+use core::time::Duration;
+use std::{env, net::SocketAddr, path::PathBuf};
+
+use clap::{self, Parser};
+use libafl::observers::ExplicitTracking;
+use libafl::{
+    corpus::{Corpus, InMemoryCorpus, OnDiskCorpus},
+    events::{launcher::Launcher, EventConfig},
+    executors::{inprocess::InProcessExecutor, ExitKind},
+    feedback_or, feedback_or_fast,
+    feedbacks::{CrashFeedback, MaxMapFeedback, TimeFeedback, TimeoutFeedback},
+    fuzzer::{Fuzzer, StdFuzzer},
+    inputs::{BytesInput, HasTargetBytes},
+    monitors::{MultiMonitor, OnDiskTomlMonitor},
+    mutators::{
+        havoc_mutations::havoc_mutations,
+        scheduled::{tokens_mutations, StdScheduledMutator},
+        token_mutations::Tokens,
+    },
+    observers::{CanTrack, HitcountsMapObserver, TimeObserver},
+    schedulers::{IndexesLenTimeMinimizerScheduler, SetcoverScheduler},
+    stages::mutational::StdMutationalStage,
+    state::{HasCorpus, HasSetCover, StdState},
+    Error, HasMetadata,
+};
+use libafl_bolts::{
+    core_affinity::Cores,
+    rands::StdRand,
+    shmem::{ShMemProvider, StdShMemProvider},
+    tuples::{tuple_list, Merge},
+    AsSlice,
+};
+use libafl_targets::{libfuzzer_initialize, libfuzzer_test_one_input, std_edges_map_observer};
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+/// Parse a millis string to a [`Duration`]. Used for arg parsing.
+fn timeout_from_millis_str(time: &str) -> Result<Duration, Error> {
+    Ok(Duration::from_millis(time.parse()?))
+}
+
+/// The commandline args this fuzzer accepts
+#[derive(Debug, Parser)]
+#[command(
+    name = "libfuzzer_binutils_launcher",
+    about = "A libfuzzer-like fuzzer for binutils with llmp-multithreading support and a launcher",
+    author = "Andrea Fioraldi <andreafioraldi@gmail.com>, Dominik Maier <domenukk@gmail.com>"
+)]
+struct Opt {
+    #[arg(
+    short,
+    long,
+    value_parser = Cores::from_cmdline,
+    help = "Spawn clients in each of the provided cores. Broker runs in the 0th core. 'all' to select all available cores. 'none' to run a client without binding to any core. eg: '1,2-4,6' selects the cores 1,2,3,4,6.",
+    name = "CORES"
+    )]
+    cores: Cores,
+
+    #[arg(
+        long,
+        help = "Spawn n clients on each core, this is useful if clients don't fully load a client, e.g. because they `sleep` often.",
+        name = "OVERCOMMIT",
+        default_value = "1"
+    )]
+    overcommit: usize,
+
+    #[arg(
+        short = 'p',
+        long,
+        help = "Choose the broker TCP port, default is 1337",
+        name = "PORT",
+        default_value = "1337"
+    )]
+    broker_port: u16,
+
+    #[arg(short = 'a', long, help = "Specify a remote broker", name = "REMOTE")]
+    remote_broker_addr: Option<SocketAddr>,
+
+    #[arg(
+        short,
+        long,
+        help = "Set an initial corpus directory",
+        name = "INPUT",
+        required = true
+    )]
+    input: Vec<PathBuf>,
+
+    #[arg(
+        short,
+        long,
+        help = "Set the output directory, default is ./out",
+        name = "OUTPUT",
+        default_value = "./out"
+    )]
+    output: PathBuf,
+
+    #[arg(
+    value_parser = timeout_from_millis_str,
+    short,
+    long,
+    help = "Set the exeucution timeout in milliseconds, default is 10000",
+    name = "TIMEOUT",
+    default_value = "10000"
+    )]
+    timeout: Duration,
+    /*
+    /// This fuzzer has hard-coded tokens
+    #[arg(
+
+        short = "x",
+        long,
+        help = "Feed the fuzzer with an user-specified list of tokens (often called \"dictionary\"",
+        name = "TOKENS",
+        multiple = true
+    )]
+    tokens: Vec<PathBuf>,
+    */
+}
+
+/// The main fn, `no_mangle` as it is a C symbol
+#[no_mangle]
+pub extern "C" fn libafl_main() {
+    // Registry the metadata types used in this fuzzer
+    // Needed only on no_std
+    // unsafe { RegistryBuilder::register::<Tokens>(); }
+    let opt = Opt::parse();
+    println!("Entry point");
+
+    let broker_port = opt.broker_port;
+    let cores = opt.cores;
+
+    println!(
+        "Workdir: {:?}",
+        env::current_dir().unwrap().to_string_lossy().to_string()
+    );
+
+    let shmem_provider = StdShMemProvider::new().expect("Failed to init shared memory");
+
+    let monitor = OnDiskTomlMonitor::new(
+        "./fuzzer_stats.toml",
+        MultiMonitor::new(|s| println!("{s}")),
+    );
+    println!("monitor");
+
+    let mut run_client = |state: Option<_>, mut restarting_mgr, _client_description| {
+        println!("run_client");
+        // Create an observation channel using the coverage map
+        let edges_observer: ExplicitTracking<_, true, false> =
+            HitcountsMapObserver::new(unsafe { std_edges_map_observer("edges") }).track_indices();
+
+        // Create an observation channel to keep track of the execution time
+        let time_observer = TimeObserver::new("time");
+        println!("Making a new observer");
+
+        // Feedback to rate the interestingness of an input
+        // This one is composed by two Feedbacks in OR
+        let mut feedback = feedback_or!(
+            // New maximization map feedback linked to the edges observer and the feedback state
+            MaxMapFeedback::new(&edges_observer),
+            // Time feedback, this one does not need a feedback state
+            TimeFeedback::new(&time_observer)
+        );
+
+        // A feedback to choose if an input is a solution or not
+        let mut objective = feedback_or_fast!(CrashFeedback::new(), TimeoutFeedback::new());
+
+        // If not restarting, create a State from scratch
+        let mut state = state.unwrap_or_else(|| {
+            StdState::new(
+                // RNG
+                StdRand::new(),
+                // Corpus that will be evolved, we keep it in memory for performance
+                OnDiskCorpus::new(&opt.output).unwrap(),
+                // Corpus in which we store solutions (crashes in this example),
+                // on disk so the user can get them after stopping the fuzzer
+                OnDiskCorpus::new(&opt.output).unwrap(),
+                // States of the feedbacks.
+                // The feedbacks can report the data that should persist in the State.
+                &mut feedback,
+                // Same for objective feedbacks
+                &mut objective,
+            )
+            .unwrap()
+        });
+
+        println!("We're a client, let's fuzz :)");
+
+        // Create a directory of object files if not already present
+        if state.metadata_map().get::<Tokens>().is_none() {
+            // tl; dr
+            state.add_metadata(Tokens::from([
+                // a sample object file
+                vec![0x7f, 0x45, 0x4c, 0x46, 0x02, 0x01, 0x01, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x01, 0x00, 0x3e, 0x00, 0x01, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x20, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x40, 0x00, 0x0a, 0x00, 0x09, 0x00],
+                vec![0x00, 0x47, 0x43, 0x43, 0x3a, 0x20, 0x28, 0x55],
+                vec![0x62, 0x75, 0x6e, 0x74, 0x75, 0x20, 0x31, 0x31],
+                vec![0x2e, 0x34, 0x2e, 0x30, 0x2d, 0x31, 0x75, 0x62],
+                vec![0x75, 0x6e, 0x74, 0x75, 0x31, 0x7e, 0x32, 0x32],
+                vec![0x2e, 0x30, 0x34, 0x29, 0x20, 0x31, 0x31, 0x2e],
+                vec![0x34, 0x2e, 0x30, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x04, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00],
+                vec![0x05, 0x00, 0x00, 0x00, 0x47, 0x4e, 0x55, 0x00],
+                vec![0x02, 0x00, 0x00, 0xc0, 0x04, 0x00, 0x00, 0x00],
+                vec![0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x01, 0x00, 0x00, 0x00, 0x04, 0x00, 0xf1, 0xff],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x61, 0x2e, 0x63, 0x00, 0x00, 0x2e, 0x73],
+                vec![0x79, 0x6d, 0x74, 0x61, 0x62, 0x00, 0x2e, 0x73],
+                vec![0x74, 0x72, 0x74, 0x61, 0x62, 0x00, 0x2e, 0x73],
+                vec![0x68, 0x73, 0x74, 0x72, 0x74, 0x61, 0x62, 0x00],
+                vec![0x2e, 0x74, 0x65, 0x78, 0x74, 0x00, 0x2e, 0x64],
+                vec![0x61, 0x74, 0x61, 0x00, 0x2e, 0x62, 0x73, 0x73],
+                vec![0x00, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x65, 0x6e],
+                vec![0x74, 0x00, 0x2e, 0x6e, 0x6f, 0x74, 0x65, 0x2e],
+                vec![0x47, 0x4e, 0x55, 0x2d, 0x73, 0x74, 0x61, 0x63],
+                vec![0x6b, 0x00, 0x2e, 0x6e, 0x6f, 0x74, 0x65, 0x2e],
+                vec![0x67, 0x6e, 0x75, 0x2e, 0x70, 0x72, 0x6f, 0x70],
+                vec![0x65, 0x72, 0x74, 0x79, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x1b, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00],
+                vec![0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x21, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00],
+                vec![0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x27, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00],
+                vec![0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x2c, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00],
+                vec![0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x2c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x35, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x6c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x45, 0x00, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00],
+                vec![0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x90, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x08, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00],
+                vec![0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x18, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x09, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x11, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0xc5, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x58, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+            ]));
+        }
+
+        // Setup a basic mutator with a mutational stage
+        let mutator = StdScheduledMutator::new(havoc_mutations().merge(tokens_mutations()));
+        let mut stages = tuple_list!(StdMutationalStage::new(mutator));
+
+        // A setcover scheduler instance.
+        let mut setcover_sched = SetcoverScheduler::new(edges_observer.as_ref());
+
+        // A minimization+queue policy to get testcasess from the corpus
+        let scheduler = IndexesLenTimeMinimizerScheduler::new(&edges_observer, setcover_sched);
+
+        // A fuzzer with feedbacks and a corpus scheduler
+        let mut fuzzer = StdFuzzer::new(scheduler, feedback, objective);
+
+        // The wrapped harness function, calling out to the LLVM-style harness
+        let mut harness = |input: &BytesInput| {
+            let target = input.target_bytes();
+            let buf = target.as_slice();
+            unsafe {
+                libfuzzer_test_one_input(buf);
+            }
+            ExitKind::Ok
+        };
+
+        // Create the executor for an in-process function with one observer for edge coverage and one for the execution time
+        #[cfg(target_os = "linux")]
+        let mut executor = InProcessExecutor::batched_timeout(
+            &mut harness,
+            tuple_list!(edges_observer, time_observer),
+            &mut fuzzer,
+            &mut state,
+            &mut restarting_mgr,
+            opt.timeout,
+        )?;
+
+        #[cfg(not(target_os = "linux"))]
+        let mut executor = InProcessExecutor::with_timeout(
+            &mut harness,
+            tuple_list!(edges_observer, time_observer),
+            &mut fuzzer,
+            &mut state,
+            &mut restarting_mgr,
+            opt.timeout,
+        )?;
+
+        // The actual target run starts here.
+        // Call LLVMFUzzerInitialize() if present.
+        let args: Vec<String> = env::args().collect();
+        if unsafe { libfuzzer_initialize(&args) } == -1 {
+            println!("Warning: LLVMFuzzerInitialize failed with -1");
+        }
+
+        // In case the corpus is empty (on first run), reset
+        if state.must_load_initial_inputs() {
+            state.use_setcover_schedule();
+            state
+                .load_initial_inputs_forced(
+                    &mut fuzzer,
+                    &mut executor,
+                    &mut restarting_mgr,
+                    &opt.input,
+                )
+                .unwrap_or_else(|_| panic!("Failed to load initial corpus at {:?}", &opt.input));
+            println!("We imported {} inputs from disk.", state.corpus().count());
+
+            let count = state.corpus().count();
+            assert!(count > 0);
+        }
+
+        fuzzer.fuzz_loop(&mut stages, &mut executor, &mut state, &mut restarting_mgr)?;
+        Ok(())
+    };
+
+    match Launcher::builder()
+        .shmem_provider(shmem_provider)
+        .configuration(EventConfig::from_name("default"))
+        .monitor(monitor)
+        .run_client(&mut run_client)
+        .cores(&cores)
+        .overcommit(opt.overcommit)
+        .broker_port(broker_port)
+        .remote_broker_addr(opt.remote_broker_addr)
+        .stdout_file(Some("/tmp/fuzzer.txt"))
+        .build()
+        .launch()
+    {
+        Ok(()) => (),
+        Err(Error::ShuttingDown) => println!("Fuzzing stopped by user. Good bye."),
+        Err(err) => panic!("Failed to run launcher: {err:?}"),
+    }
+}
