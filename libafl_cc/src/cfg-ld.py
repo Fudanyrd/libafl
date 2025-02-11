28001d6f184b09271410cd9320cab8331a42ecc4
Rundong Yang <143480204+Fudanyrd@users.noreply.github.com>
2025-02-11 15:07:51 +0800

    [IMPL] cfg-graph linker

---
+++ libafl_cc/src/cfg-ld.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+import json
+import os
+import sys
+
+class CFGLinker():
+  """ Linker that links output of dump-cfg-pass into a single .cfg file """
+  def __init__(self):
+    self.calls = dict()
+    self.edges = dict()
+    self.entries = dict()
+
+  def add_cfg_file(self, file_name):
+    """ Add a cfg file """
+    print("\033[01;92m[+]\033[0;m cfg-ld: linking", file_name)
+    if not os.path.exists(file_name):
+      self.error(f"{file_name}: No such file or directory")
+    try:
+      with open(file_name, 'r') as fobj:
+        obj = json.load(fobj)
+        self.add_object(obj)
+      del obj
+    except Exception as e:
+      self.error(f"{file_name}: {e}.")
+  
+  def add_object(self, obj: dict):
+    for key in ['calls', 'entries', 'edges']:
+      if key not in obj.keys():
+        raise RuntimeError(f"key {key} not found.")
+    
+    new_calls = obj['calls']
+    new_edges = obj['edges']
+    new_entries = obj['entries']
+
+    for key in new_calls:
+      if key in self.calls.keys():
+        raise RuntimeError(f"duplicate key {key} found in \'calls\'")
+      else:
+        self.calls[key] = new_calls[key]
+    
+    for key in new_edges:
+      if key in self.edges.keys():
+        raise RuntimeError(f"duplicate key {key} found in \'edges\'")
+      else:
+        self.edges[key] = new_edges[key]
+      
+    for key in new_entries:
+      if key in self.entries.keys():
+        # raise RuntimeError(f"duplicate key {key} found in \'entries\'")
+        pass
+      else:
+        self.entries[key] = new_entries[key]
+    
+    del new_calls
+    del new_entries
+    del new_edges
+  
+  def error(self, message: str):
+    """ Print a error message and exit. """
+    print("cfg-ld: \033[01;31merror:\033[0m", message)
+    raise RuntimeError("Further execution is not possible.")
+  
+  def output(self, file_name = "a.out.cfg"):
+    """
+    Print linker output to file_name.
+    """
+    obj = {
+      "calls": self.calls,
+      "edges": self.edges,
+      "entries": self.entries,
+    }
+    with open(file_name, 'w') as fobj:
+      json.dump(obj, fobj)
+    del obj
+
+CFG_EXTENSION = ".cfg"
+OJBECT_EXTENSION = ".o"
+
+if __name__ == "__main__":
+  args = sys.argv[1:]
+  print("\033[01;32m[*]\033[0;m cfg-ld", args)
+  linker: CFGLinker = CFGLinker()
+  if len(args) == 0:
+    linker.error("No input files")
+
+  # filter args
+
+  # parser args
+  i = 0
+  input_files = []
+  output_file = "a.out.cfg"
+  while i < len(args):
+    if args[i] == "-o" or args[i] == "--output":
+      output_file = args[i + 1]
+      i += 2
+    else:
+      input_files.append(args[i])
+      i += 1
+
+  for input_file in input_files:
+    linker.add_cfg_file(input_file)
+  linker.output(file_name = output_file) 
+  sys.exit(0)
