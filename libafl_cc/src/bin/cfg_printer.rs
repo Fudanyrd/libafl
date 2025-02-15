ab76a77365b298fda6bc8d31d2a775785bf0a4a6
Rundong Yang <143480204+Fudanyrd@users.noreply.github.com>
2025-02-15 16:25:50 +0800

    [IMPL] cc, cxx, ar

---
+++ libafl_cc/src/bin/cfg_printer.rs
@@ -0,0 +1,21 @@
+use std::env;
+
+use libafl_cc::{CfgEdge, ControlFlowGraph, HasWeight};
+
+pub fn main() {
+  let args: Vec<String> = env::args().collect();
+    struct TestMetadata {}
+
+    impl HasWeight<TestMetadata> for TestMetadata {
+        fn compute(_metadata: Option<&TestMetadata>) -> u32 {
+            1
+        }
+    }
+
+  for i in 1..args.len() {
+    let file = &args[i];
+    let _cfg = ControlFlowGraph::<TestMetadata>::from_file(&file);
+    
+    println!("{} is a valid file", file);
+  }
+}
