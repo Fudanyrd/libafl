91f81f6e9461db89d11fce1545a1d2987556261a
Rundong Yang <143480204+Fudanyrd@users.noreply.github.com>
2026-01-27 00:48:32 +0800

    [DBG] same as last commit

--- fuzzers/rt/env/fuzzer.py
+++ fuzzers/rt/env/fuzzer.py
@@ -130,7 +130,7 @@ def build():
     cwd = os.getcwd()
     os.chdir(os.environ['OUT'])
     subprocess.check_call(['/usr/lib/build-cfg.sh', fuzz_target_exe],
-                          stdout=open('compile.log', 'w'))
+                          stderr=open('compile.log', 'w'))
     os.chdir(cwd)
 
 
